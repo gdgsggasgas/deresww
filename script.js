8edaa4068fe84b5fcdbb7e34155773c6567477b1
gdgsggasgas <154766301+gdgsggasgas@users.noreply.github.com>
2024-09-24 18:41:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Add smooth scrolling to all links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
